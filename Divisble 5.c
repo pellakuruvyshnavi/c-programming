512643cbd2b51eb3edb98296ba6dc20f4867bbe0
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2025-12-29 13:10:04 +0530

    Add files via upload
    Divisble by 5

---
+++ Divisble 5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a=14;
+    int b=25;
+    scanf("%d,%d",&a,&b);
+    printf("Divisble %d", a);
+    if (a>b);
+    printf("not divisble %d",b);
+    
+    
+    return 0;
+}
