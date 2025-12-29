8486c451af90f629a448d39c16ee6824dcd1f3aa
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2025-12-29 11:50:05 +0530

    Add files via upload
    even or odd

---
+++ even or odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() { 
+    int num;
+    scanf("%d",&num);
+    printf("Enter a number: ");
+    if ( num%2==0)
+    printf("Even");
+    else 
+    printf("Odd");
+    
+    return 0;
+}
