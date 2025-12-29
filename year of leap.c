50c47e2042ff55eab013a464c726bde7cf151420
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2025-12-29 12:36:49 +0530

    Add files via upload
    leap of year

---
+++ year of leap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a= 2023;
+    int b=2024;
+    scanf("%d,%d",&a,&b);
+    if (a>b)
+    
+    
+    printf("Enter a leap year");
+    else 
+    printf("Enter a not leap year");
+    
+    return 0;
+}
+
