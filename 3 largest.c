7f7ac6a440c95500802057fd85e7a8ea59f7a49d
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2025-12-29 12:35:55 +0530

    Add files via upload
    three largest

---
+++ 3 largest.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d,%d,%d",&a,&b,&c);
+    if (a>b)
+    printf ("largest number %d ", a  );
+else if (b>c)
+ printf ("largest number%d", b) ;
+ else 
+ printf("largest number  %d", c);
+    return 0;
+}
