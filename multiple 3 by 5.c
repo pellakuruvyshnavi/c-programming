17574350eaac61e3b3c0c952197a7721f4905e12
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2025-12-30 15:47:10 +0530

    Add files via upload
    multiple 3 and 5

---
+++ multiple 3 by 5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int main()  {
+        int a=3;
+        int b=5;
+    scanf("%d,%d",&a,&b);
+    if (a%3==0 && b%5==0)
+    printf( "Yes");
+    
+    else 
+        printf("No");
+    } 
+    return 0;
+}
