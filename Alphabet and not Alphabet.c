73aa531c6253381e80dbc6802ffb94d1ea7f68b6
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2025-12-31 14:44:26 +0530

    Add files via upload
    Alphabet and no Alphabet

---
+++ Alphabet and not Alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    if((ch>='A' &&ch<='Z')||(ch>='a'&& ch<='z'))
+    printf("Alphabet");
+    else 
+    printf("Not Aiphabet");
+    
+    return 0;
+}
