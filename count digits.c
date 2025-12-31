8de967586234cc80a922b6bc7f647947fd831a37
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2025-12-31 22:20:21 +0530

    Add files via upload
    count digits

---
+++ count digits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i ,n;
+    scanf("%d",&n);
+    printf("enter a number ");
+
+    printf("count digits in a number for %d",n);
+    for (i=1;i<=10;i++){
+
+        printf("%d",n);
+    }
+    return 0;
+}
