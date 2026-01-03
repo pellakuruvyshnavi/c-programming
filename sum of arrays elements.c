6427ffb6cd63ad6c5ddab34a10e47a9f54bf9309
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2026-01-03 17:22:29 +0530

    Add files via upload
    sum of arrays elements

---
+++ sum of arrays elements.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i,n,a[i],sum=10;
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++){
+
+      sum+=a[i];
+    }
+        printf("%d",sum);
+    
+      return 0;
+}
