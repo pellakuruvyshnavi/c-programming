031e9fe30d46e1e057e0aa9670851fe209e3277f
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2026-01-04 17:35:06 +0530

    Add files via upload
    maximum arrays elemn[ent

---
+++ maximum elements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int  max,i,a[i];
+    printf("enter a size of array");
+    scanf("%d",&a[i]);
+    
+    for(i=0;i<max;i++){
+    
+        scanf("%d",a[i]);
+    }
+    
+   { max=a[i];
+    printf("maximum element=%d",max);
+}
+    return 0;
+}
