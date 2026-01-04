7c8fff76e0614b7761f3896a9cdac96373c6c61d
pellakuruvyshnavi <pellakuruvyshnavi@gmail.com>
2026-01-04 17:35:49 +0530

    Add files via upload
    minimum element

---
+++ minimum element.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int  min,i,a[i];
+    printf("enter a size of array");
+    scanf("%d",&a[i]);
+    
+    for(i=0;i<min;i++){
+    
+        scanf("%d",a[i]);
+    }
+    
+   { min=a[i];
+    printf("minimum element=%d",min);
+}
+    return 0;
+}
