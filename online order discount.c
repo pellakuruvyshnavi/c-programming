5aa8bbd185cd982a09598a04a3e5d1e8e787ff28
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-10 17:45:49 +0530

    Add files via upload
    online order  discount if

---
+++ online order discount.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>=1500){
+        printf("Discount applied");
+    }
+
+    printf("no  Discount applied");
+    
+    return 0;
+}
