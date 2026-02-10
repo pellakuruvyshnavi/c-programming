c56f57fdf20944f44de41479566d45653fa7806e
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-10 17:43:49 +0530

    Add files via upload
    salary bonus eligibility

---
+++ salary bonus eligibility.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+   int n;
+   scanf("%d",&n);
+   if(n>28000){
+    printf("Eligible bonus");
+   }
+    else {
+        printf("not eligible bonus");
+    }
+
+    
+
+}
