8d0aee0fa61397773f2b982599b9436f3e8572c3
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-11 15:26:51 +0530

    Add files via upload
    voting eligibility

---
+++ voting eligibility.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+     int age;
+     scanf("%d",&age);
+     if (age>=18){
+        printf("eligible vote ");
+     }
+     else {     
+        printf("no eligible vote");
+}
+     
+    
+    return 0;
+}
