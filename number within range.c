68740f66d236a87c04be308d7cf9e7aa771475ed
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-11 17:22:37 +0530

    Add files via upload
    Number within range

---
+++ number within range.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>=10 && n<=20){
+
+    printf("number within the range");
+    }
+    else {
+    printf("number not within the range");
+    }
+    
+    return 0;
+}          
