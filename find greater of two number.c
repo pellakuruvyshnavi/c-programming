9a4347f740616bd922d2fb0afd4a57241395595f
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-11 15:21:02 +0530

    Add files via upload
    Find greater of two number

---
+++ find greater of two number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>=0){
+
+        printf("greater number",n);
+    }
+    else if (n<=0){
+        printf("no greater number",n);
+    }
+    
+    return 0;
+}
