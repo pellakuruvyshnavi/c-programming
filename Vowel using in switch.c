5852480b934e8478e889a1e9a3d5b52ae927d700
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-12 20:17:05 +0530

    Add files via upload
    Vowel using in switch

---
+++ Vowel using in switch.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main() {
+    char vowel;  
+      printf("Enter a vowel");
+       scanf("%c",&vowel);
+
+    switch(vowel){
+case 'A':
+     printf("Vowel");
+     break; 
+  case 'E':
+       printf("Vowel");
+       break;
+   case 'I':
+       printf("vowel");
+       break;
+    case 'O':
+        printf("vowel ");
+        break;
+    case 'U':
+         printf("vowel");
+         break;                
+
+              
+  case 'a':
+     printf("Vowel");
+     break; 
+  case 'e':
+       printf("Vowel");
+       break;
+   case 'i':
+       printf("vowel");
+       break;
+    case 'o':
+        printf("vowel ");
+        break;
+    case 'u':
+         printf("vowel");
+         break;                
+
+    
+        
+ default:
+       printf("Enter the Vowel ");
+    }
+    
+    return 0;
+}
