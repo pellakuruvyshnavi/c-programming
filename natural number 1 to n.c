45d8cd973c15414d268a9d870afa5b8c6d5c97c1
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-13 19:21:08 +0530

    Add files via upload
    natural number 1 to n

---
+++ natural number 1 to n.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a number  ");
+    scanf("%d",&n);
+   for (int i=n; i<=5; i++){
+
+    printf("%d",i);
+   }
+
+     
+    
+    return 0;
+}
