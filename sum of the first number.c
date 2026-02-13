27ca1a2adca302d9d9c344be34a7045e763b1726
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-13 19:22:07 +0530

    Add files via upload
    sum of the first number

---
+++ sum of the first number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n,sum;
+   printf("Enter a number  ");
+   scanf("%d",&n);
+    for (int i = 1; i <= n; i++){
+
+        sum = sum +i;
+        printf("%d",n);
+    }
+       
+    
+    return 0;
+}
