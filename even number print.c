0de3f7ee30645456ca0810495a566f24e75ef09a
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-02-13 19:23:06 +0530

    Add files via upload
    even number print

---
+++ even number print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a number  ");
+    scanf("%d",&n);
+    for (int i=2;i<=20;i+=2){
+        printf("%d\n",i);
+    }
+
+
+
+    
+    return 0;
+}
