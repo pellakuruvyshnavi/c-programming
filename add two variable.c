cc69cf00da625d9ca7cb8c37bd4938ea02cd33a9
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-03-07 16:19:34 +0530

    Add files via upload
    add two variable

---
+++ add two variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int  sum,a, b;
+    
+    printf("enter a= ");
+    scanf("%d",&a);
+    printf("enter b= ");
+    scanf("%d",&b);
+    sum= a+b;
+    printf("%d",sum);
+    
+    return 0;
+}
