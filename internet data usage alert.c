74b3606488fd3093eeb86fb5a293356f41302256
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-03-16 20:02:46 +0530

    Add files via upload

---
+++ internet data usage alert.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%s",a);
+    if(a>2.5){
+        printf("Data limits");
+    }
+    else {
+        printf("Data within limits");
+    }
+
+    
+    return 0;
+}
