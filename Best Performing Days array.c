ec732b46753e77705894330a6ce440bcdfee7a2a
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-03-24 21:01:39 +0530

    Add files via upload

---
+++ Best Performing Days array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for (i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    
+    }
+    int a[n];
+    int index=0;
+    a[n]=a[n-1];
+    index=1;
+    for(i=n-1;i<n;i++){
+        if(a[i]=n-1){
+            a[i]>a[n-1];
+        }
+    }
+    printf("%d",index);1
+    
+    return 0;
+}
