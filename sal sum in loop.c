24c09e6e048a391cb81800217db29744cd1b51a2
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-04-08 22:58:38 +0530

    Add files via upload

---
+++ sal sum in loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int day;
+    scanf("%d",&day);
+    int sal;
+    for(int i=0;i<n;i++){
+         sal=n-(day*100);
+        
+    }printf("%d",sal);
+
+    
+    return 0;
+}
