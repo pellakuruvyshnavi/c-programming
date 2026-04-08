7c707ace40acb40561a65937ffb7523045223e0e
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-04-08 22:58:11 +0530

    Add files via upload

---
+++ add three integer and print it result.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int  sum,a=2,b=3,c=5;
+    
+     printf("enter a= ");
+     scanf("%d",&a);
+
+     printf("enter b= ");
+     scanf("%d",&b);
+
+     printf("enter c=  ");
+     scanf("%d",&c);
+
+     sum= a+b+c;
+     printf("%d",sum);
+    return 0;
+} 
