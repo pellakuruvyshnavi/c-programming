f39df6eaac3ec058be6c42946bdadc081ed3bb10
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-05-10 23:29:09 +0530

    Add files via upload

---
+++ count prime for loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    
+    int count =0;
+    int i=2;
+    int n;
+    printf("Enter a number  ");
+    while (count==n){
+        if( isprime(i)){
+            printf("%d",i);
+            count++;
+        }
+    }
+    int main (){
+        int n;
+        scanf("%d",&n);
+     for(int =2;i<=n;i++){
+        if(isprime(i)){
+            printf("%d",i);
+        }
+     }   
+    }
+    
+    return 0;
+}
