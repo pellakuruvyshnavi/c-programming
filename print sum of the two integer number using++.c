3b66adb17619fefcc4688b7def1b722ec25901a5
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-05-19 20:37:18 +0530

    Add files via upload

---
+++ print sum of the two integer number using++.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int c ,a=8,b=3;
+    printf("enter a=  ");
+    scanf("%d",&a);
+    printf("enter b=  ");
+    scanf("%d",&b);
+    c =a+b;
+    printf("%d",c);
+    
+    return 0;
+}
