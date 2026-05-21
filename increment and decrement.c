9e378d4168916dd23216c7a02200372d5c0c9987
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-05-21 10:31:34 +0530

    Add files via upload
    increment and decrement

---
+++ increment and decrement.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main() {
+     
+     int a,b,c;
+      a=10;
+
+      printf ("the value of a BEFORE INCREMENT IS %d",a);
+     /*increment A*/
+     a=a+1;
+     printf ("the value of BEFORE increment is %d",a);
+    a=a-1;
+    printf ("the value of BEFORE Decrement is %d",a);
+
+
+     
+    return 0;
+}
