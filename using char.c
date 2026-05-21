6998de3e357e37074b074516e7c72e11e1ff85d9
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-05-21 10:07:17 +0530

    Add files via upload

---
+++ using char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+       char name [10];
+       gets (name );
+       puts(name ) ;  
+   
+  printf("The name you typed is %s",strupr(name));
+
+
+
+    return 0;
+}
