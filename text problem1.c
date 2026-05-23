c0f47484749542dbb901af50145e7007a901fc33
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-05-23 16:29:36 +0530

    Add files via upload

---
+++ text problem1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+        float pI=3.14;
+         int area,r;
+         printf("ENTETR the radius: \n");
+         scanf("%d",&r);
+         area= pI*r*r;
+         printf("\nArea of the circle =%d",area);
+  
+  
+    
+    return 0;
+}
