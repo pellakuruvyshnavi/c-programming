cff4c97a61bb686e4f76f9a2edf5f816503de2d3
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-06-01 18:56:13 +0530

    Add files via upload

---
+++ character s q4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    char ch='a';
+    printf("ch:%c\n",ch);
+    char m='2';
+    printf("m:%c\n",m);
+    char n='_';
+    printf("n p=%c\n",n);
+    char d[]=".6";
+    printf("the decimal version:%s\n",d);
+    
+    //printf("%c\n",arra);
+         printf("ch*m:%c%c%c\n",ch,m,ch);
+         char g []="name";
+         printf("g:%s\n",g);
+         char h[]="123445";
+         printf("123456677:%s\n",h);
+         char f[]="asdfghh(*&)";
+         printf("asdfghjjk:%s\n",f);
+         printf("12345,asdfgh:%s%s\n",h,g);
+         char j[]="*&^";
+         printf("123,asd,#$:%s%s%s\n",h,f,j);
+         char nam[]="vyshu@gmail.com";
+         printf("%s\n",nam);
+         char z='"';
+         printf("%c\n",z);
+
+
+    return 0;
+}
