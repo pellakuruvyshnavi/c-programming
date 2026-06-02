8e291cdcca303ff364b3ec3fbb36d399ad46aec3
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-06-02 18:05:54 +0530

    Add files via upload

---
+++ c program tr 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+
+     printf("its about a story \n ");
+     printf ("vyshu\n");
+     printf ("2007\n");
+     printf("given about me \n");
+     printf("on  my day 9/04/25\n");
+     printf("its my day\n");
+     printf(" 2025 is the bad");
+    return 0;
+}
