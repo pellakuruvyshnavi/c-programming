78cf90410aee2dcabedb150ae0cd76231e5f537a
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-06-07 16:03:09 +0530

    Add files via upload

---
+++ datatypes with scanf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+     
+     int A,B,Answer;
+     printf ("ENTER A VALUE FOR A : \n ");
+     scanf ("%d",&A);
+     printf ("-----------------------\n");
+     printf ("HELLO ,MY FRIEND YOU ENTERED %d: \n ",A);
+
+
+     printf ("ENTER A VALUE FOR B : \n ");
+     scanf ("%d",&B);
+     printf ("-----------------------\n");
+     printf ("HELLO ,MY FRIEND YOU ENTERED %d: \n ",B);
+
+
+
+
+
+   
+
+   return 0 ;
+}
