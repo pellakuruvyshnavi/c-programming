7a172fa40e0df9c914517c7d9034f0041650ffe7
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-06-12 18:22:14 +0530

    Add files via upload

---
+++ ascii values.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+/*char c;
+printf("\n\n");
+for(c=65;c<=122;c=c+1) {
+    if (c>90 &&c<97){
+        continue;
+    }
+        printf("|%4d -%c",c,c);
+
+
+    
+}   
+
+ printf("\n");*/
+
+
+
+ char name [25]="PELLAKURU VYSHNAVI REDDY";
+ printf("The length of the name is %d\n",strlen(name));
+ return 0;
+}
