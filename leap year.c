c62079b1bc3222ea8353146e27067425774ef509
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-07-16 12:50:26 +0530

    Add files via upload

---
+++ leap year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int a;
+    int b;
+    scanf("%d,%d",&a,&b);
+    if (a>b) {
+
+    
+    printf("give  a year");
+    }
+    else if {
+    printf("enter a leap year");
+    }
+    else {
+    printf("enter a not leap year");
+    }
+    return 0;
+}
