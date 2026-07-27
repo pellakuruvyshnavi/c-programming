0d65dfc4980086c170cd13bfc64b85e989fcfd58
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-07-27 15:27:40 +0530

    Add files via upload

---
+++ product two integer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int c ,a=1,b=20;
+    printf("enter a=  ");
+    scanf("%d",&a);
+    printf("enter b=  ");
+    scanf("%d",&b);
+    c =a*b;
+    printf("%d",c);
+    
+    return 0;
+}
