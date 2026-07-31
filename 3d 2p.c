4657b80eff0c41c9f19980627d19c87c38a4079a
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-07-31 16:39:28 +0530

    Add files via upload

---
+++ 3d 2p.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    printf("postive");
+    else if (n<0)
+    printf("negative");
+    else
+    printf("zero");
+    
+    return 0;
+}
