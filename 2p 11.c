ba98178bcd4b831af0e27c71b015008fd698aa3f
Vyshnavi _ Reddy <pellakuruvyshnavi@gmail.com>
2026-08-28 18:26:48 +0530

    Add files via upload

---
+++ 2p 11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>=-9 && a<=9)
+    printf("Single integral");
+    else 
+    printf("Not single integral");
+    
+        return 0;
+}
